48f2e132c2284f977323f7c4169d44aa53e8665b
Nabila A'idah Diani <110476969+nabilaaidah@users.noreply.github.com>
2023-06-05 13:28:27 +0700

    Delete crud_upload directory

--- crud_upload/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost"; // Nama hostnya
-$username = "root"; // Username
-$password = "phpmyadmin"; // Password (Isi jika menggunakan password)
-$database = "mynotescode"; // Nama databasenya
-// Koneksi ke MySQL dengan PDO
-$pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
-?>
